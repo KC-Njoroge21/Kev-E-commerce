f63fff470ab689dbfe81d5f572f2b30fc709feb9
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-03-13 12:09:11 +0300

    Created the authSlice and the login reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import SignIn from './pages/SignIn'
 import Products from './pages/Products'
 import {Routes, Route} from "react-router-dom"
+import { useSelector } from 'react-redux'
 
 const App = () => {
+
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+
   return (
     <div>
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={isLoggedIn ? <Products /> : <SignIn />} />
       </Routes>
 
     </div>
