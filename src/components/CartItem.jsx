ad62a6f1e87c1f1dd255cd9b61b80efafe0453ee
KC-Njoroge21 <kevinnjorogesic@gmail.com>
2026-03-16 09:15:01 +0300

    Created and Styled the Order Summary component

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -6,42 +6,39 @@ import { GoTrash } from "react-icons/go";
 const CartItem = (props) => {
   return (
     <div className="flex p-4 gap-4 border-b border-gray-300">
+      
       <div className="w-25 h-25 p-2 shadow-lg rounded-lg">
-        <img className="w-full h-full object-cover" src={props.item.image} alt="Item img" />
+        <img
+          className="w-full h-full object-cover"
+          src={props.item.image}
+          alt="Item img"
+        />
       </div>
 
-      
-        <div className="flex w-full flex-col">
-        <div >
+      <div className="flex w-full flex-col">
+        <div>
           <h2 className="text-lg font-semibold">{props.item.name}</h2>
         </div>
 
         <div className="flex justify-between  h-full ">
-           <div className="flex gap-4 items-center  w-full">
-          <button className="p-2 shadow-lg rounded-lg border border-gray-200"><FiMinus /></button>
-          <h2>{props.item.quantity}</h2>
-          <button className="p-2 shadow-lg rounded-lg border border-gray-200" ><LuPlus /></button>
+          <div className="flex gap-4 items-center  w-full">
+            <button className="p-2 shadow-lg rounded-lg border border-gray-200">
+              <FiMinus />
+            </button>
+            <h2>{props.item.quantity}</h2>
+            <button className="p-2 shadow-lg rounded-lg border border-gray-200">
+              <LuPlus />
+            </button>
+          </div>
+
+          <div className="w-full flex items-center gap-4 justify-end">
+            <h2 className="text-lg font-semibold">${props.item.price}</h2>
+            <button>
+              <GoTrash size={25} />
+            </button>
+          </div>
         </div>
-
-        
-        <div className="w-full flex items-center gap-4 justify-end">
-          <h2 className="text-lg font-semibold">${props.item.price}</h2>
-          <button>
-            <GoTrash size={25} />
-          </button>
-        </div>
-
-
-        </div>
-
-        </div>
-
-       
-
-      
-        
-      
-     
+      </div>
     </div>
   );
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,22 +1,65 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import CartItem from '../components/CartItem'
+import React from "react";
+import { useSelector } from "react-redux";
+import CartItem from "../components/CartItem";
 
 const Cart = () => {
-
-  const cartList = useSelector(state => state.cart.cartList)
+  const cartList = useSelector((state) => state.cart.cartList);
 
   const allCartItems = cartList.map((item, index) => {
-    return (
-      <CartItem item={item} key={index} />
-    )
-  })
+    return <CartItem item={item} key={index} />;
+  });
 
   return (
-    <div className='flex flex-col gap-4 p-6'>
-      {allCartItems}
-    </div>
-  )
-}
+    <section>
+      <header>
+        <div>
+          <button></button>
+        <h2>Shopping Cart</h2>
+        </div>
+        <div>
+          <h4>4 Items</h4>
+        </div>
+      </header>
 
-export default Cart
+      <div className="flex lg:flex-row flex-col gap-5 p-6">
+       <div className="shadow border-2 border-gray-200 rounded-xl">
+         <h2 className="text-xl font-semibold">Cart Items</h2>
+      <div className="flex flex-col gap-4 p-6">{allCartItems}</div>
+       </div>
+
+       <div className="flex flex-col gap-5 p-5 shadow-lg border-2 h-fit border-gray-200 rounded-xl">
+          <h3 className="text-xl font-semibold">Order Summary</h3>
+
+          <div>
+            <h4 className="text-gray-500 font-semibold">Promo Code</h4>
+            <div className="flex gap-4">
+              <input className="p-2 border border-gray-300 rounded-lg w-full" type="text" placeholder="Enter code" />
+              <button className="py-2 px-4 bg-gray-100 font-semibold rounded-lg">Apply</button>
+            </div>
+            <h5 className="text-sm text-gray-500">Try code: SAVE10</h5>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="flex justify-between">
+              <h4 className="text-gray-700">Subtotal</h4>
+              <h4 className="text-gray-700">$759.95</h4>
+            </div>
+            <div className="flex justify-between">
+              <h4 className="text-gray-700">Shipping</h4>
+              <h4 className="text-gray-700">Free</h4>
+            </div>
+          </div>
+          <hr className="text-gray-300" />
+          <div className="flex justify-between">
+            <h3 className="font-semibold text-xl">Total</h3>
+            <h4 className="font-bold text-2xl">$759.95</h4>
+          </div>
+          <button className="p-3 bg-black text-white font-semibold w-full rounded-lg">Proceed to Checkout</button>
+          <hr className="text-gray-300" />
+          <h5 className="text-sm text-gray-500">Secure checkout . Free returns within 30 days</h5>
+       </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cart;
